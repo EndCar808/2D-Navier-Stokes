1e0dd7a784472348361a18fde7255ab56042c626
EndCar808 <enda.carroll@ucdconnect.ie>
2022-10-16 16:31:50 +0100

    SOLVER: Updated for running KF forcing. PLOT: Added script for plotting run info from a long run.

--- Solver/src/data_types.h
+++ Solver/src/data_types.h
@@ -25,7 +25,7 @@
 // ---------------------------------------------------------------------
 //  Compile Time Macros and Definitions
 // ---------------------------------------------------------------------
-#define checkError(x) ({int __val = (x); __val == -1 ? \
+#define checkError(x) ({int __val = (x); __val == -1 ? \	
 	({fprintf(stderr, "ERROR ("__FILE__":%d) -- %s\n", __LINE__, strerror(errno)); \
 	exit(-1);-1;}) : __val; })
 
